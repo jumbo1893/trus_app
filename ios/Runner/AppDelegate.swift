1e7ba2cb7a8cb329ea7fb674194b1056b32b7ec7
matej <jumbo1893@seznam.cz>
2026-05-08 20:23:29 +0200

    úprava push pro ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Flutter
+import UserNotifications
+import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +10,38 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+
+    application.registerForRemoteNotifications()
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+  ) {
+    Messaging.messaging().apnsToken = deviceToken
+
+    let tokenParts = deviceToken.map { data in
+      String(format: "%02.2hhx", data)
+    }
+    let token = tokenParts.joined()
+
+    print("[push-diagnostics-native] didRegisterForRemoteNotificationsWithDeviceToken apnsToken=\(token)")
+
+    super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+
+  override func application(
+    _ application: UIApplication,
+    didFailToRegisterForRemoteNotificationsWithError error: Error
+  ) {
+    print("[push-diagnostics-native] didFailToRegisterForRemoteNotificationsWithError error=\(error.localizedDescription)")
+
+    super.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
   }
 }
